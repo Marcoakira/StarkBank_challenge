ae7d5a4f6c80dabffe095c0f61523db1e2d75a6f
Marco Aurélio Menezes <tetissu2@gmail.com>
2022-02-15 19:09:22 -0300

    Update webhook.py
    modo

--- the_invoices/webhook.py
+++ the_invoices/webhook.py
@@ -1,6 +1,9 @@
 import starkbank
 
-
+'''
+https://adoteumdev.postman.co/workspace/My-Workspace~3c57eae0-6ad8-4611-8bad-7e493f990edc/collection/19595432-5ce26371-6201-4e28-bb02-bc39bc3ba6c9?ctx=documentation
+Foi uma forma extremamente facil de fazer que infelizmente descobri apenas nos ultimos minutos para a entrega.
+'''
 def webhooksite():
     webhook = starkbank.webhook.create(
         url="https://webhook.site/117b85f8-2b69-4325-8c54-0d0f5eb8f0c2?",
