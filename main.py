5f74a6477186e45e0f8a0859a9819511299675ca
Marco Aurélio Menezes <tetissu2@gmail.com>
2022-02-18 23:20:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # outside
 from time import sleep
 import authentication
+from threading import Thread
 # inside
 from the_invoices.invoice import invo
-from the_invoices.invoice_transfer import the_tranfers
+from the_invoices.webhook import event_webhoot
+
+'''Thread, allows the_webhook and main functions to work simultaneously. at the same time.
+one function works every 3 hours and the other every 3 minutes. But both run at the same time.'''
 
 
 def main():
@@ -13,21 +17,22 @@ def main():
         authentication.authe()
         invo()
         sleep(3)  # avoid queue issues (could have been done on lists)
-        the_tranfers()
-        cicle = - 1
+        cicle -= 1
         sleep(10800)  # 3 hours in seconds
 
 
+def the_webhook():
+    while True:
+        event_webhoot()
+        sleep(180) #it does a check every 3 minutes
+
+
+
+
+webhook = Thread(target=the_webhook)
+my_main = Thread(target=main)
+webhook.start()
+my_main.start()
+
 if __name__ == "__main__":
     main()
-
-"""
-
-TODO next implementation: the webhook will replace the inoives_transfer. making a more complete integration
-with the webhook.site
-
-from webhook import webhooksite
-
-webhooksite()
-
-"""
